6a403c42859c586e6cb42875f55c3c68a37ef0e1
muhammad45401 <saadjani3333@gmail.com>
2023-10-23 23:23:29 +0500

    day-time

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+var time = prompt ("enter the day time");
+
+
+
+if (time <=12){
+             document.write ("Good day");
+}
+
+   else if (time <=19){
+                        document.write("Good evening");
+   } 
+
+else{
+         document.write("Good Night");}
